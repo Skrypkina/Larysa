150768fe0178dd8897f13ad37daa5d48255a3bbc
Larysa <Larysa76@gmail.com>
2018-08-13 17:13:43 +0300

    modul7 add2

---
+++ js/modul7add2/index.js
@@ -0,0 +1,40 @@
+'use strict';
+// first task
+/*
+  Есть список категорий с классом categories (на вкладке HTML).
+  
+  Напишите код, который для каждого элемента li (первая вложенность) 
+  в списке categories выведет в консоль:
+  - Текст непосредственно в нём (название категории)
+  - Количество всех вложенных в него элементов li
+  
+  К примеру:
+    Категория: Животные
+    Количество вложенных li: 4
+*/
+
+// const list = document.body.firstElementChild;
+ 
+// console.log(list.children[0].firstChild); 
+// console.log(list.children[0].firstElementChild.children.length);
+
+// second task
+
+/*
+  Дан список с классом .list
+	- Найдите первого потомка списка и сделайте его текст красного цвета
+	- Найдите последнего потомка списка и сделайте его текст синего цвета
+*/
+// const list = document.querySelector('.list');
+
+// console.log(list.firstElementChild.textContent);
+
+// list.firstElementChild.style.color = '#BF4E30';
+// list.lastElementChild.style.color = '#3066BE';
+
+
+// console.log(list);
+
+
+
+
